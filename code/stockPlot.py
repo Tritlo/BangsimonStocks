909e1d3a82b843fc3dd17197869275052ed2c35a
she5 <she5@5d233661-44a1-474e-bded-f455fa16cf90>
2013-02-03 20:09:59 +0000

    breytti stockPlot
    git-svn-id: https://tgapc93.rhi.hi.is/svn/softdev/2013/hopur19@4906 5d233661-44a1-474e-bded-f455fa16cf90

--- code/stockPlot.py
+++ code/stockPlot.py
@@ -6,19 +6,22 @@ Price = lambda x: (x[2]+x[3])/2
 
 def stockPlot(s,k):
     # Use: stockPlot(s,k)
-    # Pre: s is a stockinfo object, k is an integer, 1<=k<=7
-	# Post: We have a plot of the kth attribute of s vs time,
+    # Pre: s is a stockinfo object, k is a string which describes an attribute of s
+	# Post: We have a plot of the attribute k of s vs time,
     # where the possible attributes are, in order:
     # Open, High, Low, Close, Volume, Adj Close, and Avg. Price
     dates=[]
     yvalues=[]
     ylabels={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Volume':5, 'Adj Close':6, 'Avg. Price':7}
-    for i in range(0,len(s.infoList)-1): 
+    n=len(s.infoList)
+    for i in range(0,n-1): 
         data=s.infoList[i]
         dates.append(data[0])
         if ylabels[k]==7: yvalues.append(Price(data))
-        else: yvalues.append(data[ylabels[k]])    
-    plt.plot(dates, yvalues)
+        else: yvalues.append(data[ylabels[k]])  
+    if ylabels[k]==5: plt.hist(yvalues, n)
+    else: plt.plot(dates, yvalues)   
+    
     plt.ylabel(k)
     plt.xlabel('Date')
     # Find out how to add a title?
