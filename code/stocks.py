0837aaa3dfd4622a0c6b8ac0af43b7f505916aaf
mpg3 <mpg3@5d233661-44a1-474e-bded-f455fa16cf90>
2013-02-03 19:46:41 +0000

    Added plot to interface
    git-svn-id: https://tgapc93.rhi.hi.is/svn/softdev/2013/hopur19@4896 5d233661-44a1-474e-bded-f455fa16cf90

--- code/stocks.py
+++ code/stocks.py
@@ -1,5 +1,5 @@
 from stockInfo import stockInfo
-
+from stockPlot import stockPlot
 
 def Bhelp():
     helpstring = "The following commands are available:\n\
@@ -10,7 +10,11 @@ def Bhelp():
 
 
 def Bplot():
-
+    print "Enter number of attribute to plot:"
+    print "Available attributes are: (1) Open, (2) High, (3) Low, (4) Close, (5) Volume, (6) Adj Close, (7) Avg. Price"
+    ind = raw_input(">> ") 
+    selection = { "1": "Open", "2": "High", "3": "Low", "4": "Close", "5": "Volume", "6": "Adj Close", "7": "Avg. Price"}
+    stockPlot(stockObj,selection[ind])
 
 def Bquit():
     print "Goodbye!"
@@ -23,7 +27,7 @@ def Bquit():
 print "Welcome to the Bangsimon stock program!"
 print "Please enter the ticker of the company/index you wish to check."
 ticker = raw_input("Ticker: ")
-obj = stockInfo(ticker)
+stockObj = stockInfo(ticker)
 
 c = 'help'
 commands = {'plot': Bplot, 'help':Bhelp, 'quit':Bquit}
