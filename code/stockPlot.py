83d16913abf7683c0a186f51f6f8131187ec0b9c
she5 <she5@5d233661-44a1-474e-bded-f455fa16cf90>
2013-02-03 20:35:15 +0000

    breytti stockPlot
    git-svn-id: https://tgapc93.rhi.hi.is/svn/softdev/2013/hopur19@4915 5d233661-44a1-474e-bded-f455fa16cf90

--- code/stockPlot.py
+++ code/stockPlot.py
@@ -1,25 +1,32 @@
 from stockInfo import stockInfo
 import matplotlib.pyplot as plt
+from datetime import date
 
-# The Price function computes the average of a given day's highest and lowest prices
-Price = lambda x: (x[2]+x[3])/2
+# The Price function computes the average of a given day's opening and closing prices
+Price = lambda x: (x[1]+x[4])/2
 
-def stockPlot(s,k):
-    # Use: stockPlot(s,k)
+def stockPlot(s,k,fromDate=None,toDate=None):
+    # Use: stockPlot(s,k,fromDate,toDate)
     # Pre: s is a stockinfo object, k is a string which describes an attribute of s
+    # fromDate and toDate are optional date objects
 	# Post: We have a plot of the attribute k of s vs time,
     # where the possible attributes are, in order:
     # Open, High, Low, Close, Volume, Adj Close, and Avg. Price
+    if fromDate==None: 
+        fromDate=s.fromDate
+    if toDate=None:
+        toDate=s.toDate
     dates=[]
     yvalues=[]
     ylabels={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Volume':5, 'Adj Close':6, 'Avg. Price':7}
-    n=len(s.infoList)
+    infoList=s.listFromTo(fromDate,toDate)
+    n=len(infoList)
     for i in range(0,n-1): 
-        data=s.infoList[i]
+        data=infoList[i]
         dates.append(data[0])
         if ylabels[k]==7: yvalues.append(Price(data))
         else: yvalues.append(data[ylabels[k]])  
-    if ylabels[k]==5: plt.hist(yvalues, n)
+    if ylabels[k]==5: plt.bar(dates, yvalues, align="center")
     else: plt.plot(dates, yvalues)   
     
     plt.ylabel(k)
