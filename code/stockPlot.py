73034700780e5bafc2a5a11a5bd060979c4c6ce3
mpg3 <mpg3@5d233661-44a1-474e-bded-f455fa16cf90>
2013-02-15 15:22:01 +0000

    Breytti stockPlot til að plotta moving avg líka, og gerði smaPlot að wrapper um það.
    git-svn-id: https://tgapc93.rhi.hi.is/svn/softdev/2013/hopur19@7329 5d233661-44a1-474e-bded-f455fa16cf90

--- code/stockPlot.py
+++ code/stockPlot.py
@@ -6,57 +6,59 @@ from stockAnalysis import movingAverage
 # The Price function computes the average of a given day's opening and closing prices
 Price = lambda x: (x[1] + x[4]) / 2
 
-def stockPlot(s,k,fromDate=None,toDate=None):
+def stockPlot(s,k,fromDate=None,toDate=None, MovingAvg = False, N = 20):
     """
     # Use: stockPlot(s,k,fromDate,toDate)
     # Pre: s is a stockinfo object, k is a string which describes an attribute of s
-    # fromDate and toDate are optional date objects
-	# Post: We have a plot of the attribute k of s vs time,
+    # fromDate and toDate are optional date objects, MovingAvg is boolean
+    # N is an Integer >= 0.
+    # Post: We have a plot of the attribute k of s vs time,
     # where the possible attributes are, in order:
     # Open, High, Low, Close, Volume, Adj Close, and Avg. Price
+    # Moving Average with over the period of last N days is plotted if Moving Avg is true,
+    # though not if k is "Volume"
     """
     if fromDate==None: 
         fromDate=s.fromDate
     if toDate==None:
         toDate=s.toDate
-    dates=[]
-    yvalues=[]
-    ylabels={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Volume':5, 'Adj Close':6, 'Avg. Price':7}
-    infoList=s.listFromTo(fromDate,toDate)
-    n=len(infoList)
-    for i in range(0,n-1): 
-        data=infoList[i]
-        dates.append(data[0])
-        if ylabels[k]==7: yvalues.append(Price(data))
-        else: yvalues.append(data[ylabels[k]])  
-    if ylabels[k]==5: plt.bar(dates, yvalues, align="center")
-    else: plt.plot(dates, yvalues)   
+        
+    attrs={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Volume':5, 'Adj Close':6, 'Avg. Price':7}
     
-    plt.ylabel(k)
+    infoList=s.listFromTo(fromDate,toDate)
+
+    date=lambda d: d[0]
+    dateList=map(date,infoList)
+
+    a = attrs[k]
+    if k == 7:
+        getData =lambda d: (d[1] + d[4])/2
+    else:
+        getData =lambda d: d[a]
+
+    dataList=map(getData,infoList)
+    
+    if MovingAvg:
+        l=movingAverage(s, N, fromDate, toDate)
+        if a==5:
+            plt.bar(dateList, dataList, align="center")
+            plt.ylabel(k)
+        else:
+            plt.plot(dateList,l,'r--',dateList,dataList,'b')
+            plt.ylabel(k + " (blue) and Moving average over " + str(N) + " days (red)")
+    else:
+        if a==5:
+            plt.bar(dateList, dataList, align="center")
+        else:
+            plt.plot(dateList, dataList)   
+    
+            plt.ylabel(k)
+            
     plt.xlabel('Date')
-    # Find out how to add a title?
     plt.show()
 
 def smaPlot(s,N=20,fromDate=None,toDate=None):
-    """
-    #U: smaPlot(s,fd,td,N)
-    #Pre: s is a stockInfo object, fd, td are dates, N is an integer >=0
-    #Post: We have a plot of the moving average along with the adj. closing price vs the date from fd to td
-    """
-    if fromDate==None: 
-        fromDate=s.fromDate
-    if toDate==None:
-        toDate=s.toDate
-    date=lambda d: d[0]
-    price=lambda d: d[6]
-    infoList=s.listFromTo(fromDate,toDate)
-    dateList=map(date,infoList)
-    priceList=map(price,infoList)
-    l=movingAverage(s, N, fromDate, toDate)
-    plt.plot(dateList,l,'r',dateList,priceList,'b--')
-    plt.ylabel('Moving Average (red) and Adj. Closing Price (blue)')
-    plt.xlabel('Date')
-    plt.show()
+    stockPlot(s,"Adj Close",fromDate,toDate,True,N)
     
 if __name__ == "__main__":
     Google = stockInfo("GOOG",date(2000,1,1),date.today())
