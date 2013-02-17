24a1197febe0c53ae10fa26b92291122e604b596
she5 <she5@5d233661-44a1-474e-bded-f455fa16cf90>
2013-02-17 23:56:36 +0000

    breytti plot forriti til, nu er alltaf rautt og graent volume sulurit nedst
    git-svn-id: https://tgapc93.rhi.hi.is/svn/softdev/2013/hopur19@7786 5d233661-44a1-474e-bded-f455fa16cf90

--- code/stockPlot.py
+++ code/stockPlot.py
@@ -2,9 +2,7 @@ from stockInfo import stockInfo
 import matplotlib.pyplot as plt
 from datetime import date
 from stockAnalysis import movingAverage
-
-# The Price function computes the average of a given day's opening and closing prices
-Price = lambda x: (x[1] + x[4]) / 2
+import matplotlib
 
 def stockPlot(s,k,fromDate=None,toDate=None, MovingAvg = False, N = 20):
     """
@@ -14,53 +12,81 @@ def stockPlot(s,k,fromDate=None,toDate=None, MovingAvg = False, N = 20):
     # N is an Integer >= 0.
     # Post: We have a plot of the attribute k of s vs time,
     # where the possible attributes are, in order:
-    # Open, High, Low, Close, Volume, Adj Close, and Avg. Price
+    # Open, High, Low, Close, Adj Close, and Avg. Price
     # Moving Average with over the period of last N days is plotted if Moving Avg is true,
-    # though not if k is "Volume"
+    # Below the plot we have a bar graph of the volume of s over time, 
+    # where the bar is green if the volume is 'positive' (the day's closing price was higher than the opening price),
+    # and red if the volume is 'negative' (opening price higher than closing)
     """
     if fromDate==None: 
         fromDate=s.fromDate
     if toDate==None:
         toDate=s.toDate
         
-    attrs={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Volume':5, 'Adj Close':6, 'Avg. Price':7}
+    attrs={'Date':0, 'Open':1, 'High':2, 'Low':3, 'Close':4, 'Adj Close':5, 'Avg. Price':6}
     
     infoList=s.listFromTo(fromDate,toDate)
 
     date=lambda d: d[0]
     dateList=map(date,infoList)
 
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
     a = attrs[k]
-    if a == 7:
+    if a == 6:
         getData =lambda d: (d[1] + d[4])/2
-    else:
-        getData =lambda d: d[a]
+    else: 
+        if a==5:
+            getData =lambda d: d[6]
+        else:
+            getData=lambda d:d[a]
 
     dataList=map(getData,infoList)
     if MovingAvg:
         l=movingAverage(s, N, fromDate, toDate)
-        if a==5:
-            plt.bar(dateList, dataList, align="center")
-            plt.ylabel(k)
-        else:
-            plt.plot(dateList,l,'r--',dateList,dataList,'b')
-            plt.ylabel(k + " (blue) and Moving average over " + str(N) + " days (red)")
+        ax.plot(dateList,l,'r--',dateList,dataList,'b')
+        ax.set_ylabel(k + " (blue) and Moving average over " + str(N) + " days (red)")
     else:
-        if a==5:
-            plt.bar(dateList, dataList, align="center")
-        else:
-            plt.plot(dateList, dataList)   
+        ax.plot(dateList, dataList)   
     
-        plt.ylabel(k)
+        ax.set_ylabel(k)
 
-    plt.xlabel("Dates")
-   # plt.show()
+    ax.set_xlabel("Dates")
+
+    # shift y-limits of the plot so that there is space at the bottom for the volume bar chart
+    pad = 0.25
+    yl = ax.get_ylim()
+    ax.set_ylim(yl[0]-(yl[1]-yl[0])*pad,yl[1])
+
+    vol=lambda d: d[5]
+
+    # create the second axis for the volume bar chart
+    ax2 = ax.twinx()
+
+    # set the position of ax2 so that it is short (y2=0.32) but otherwise the same size as ax
+    ax2.set_position(matplotlib.transforms.Bbox([[0.125,0.1],[0.9,0.32]]))
+
+    # make bar charts and color differently depending on up/down for the day
+    posList=[]
+    negList=[]
+    for i in infoList:
+        if i[1]-i[4]<0:
+            posList.append(i)
+        else: negList.append(i)
+    ax2.bar(map(date,posList),map(vol,posList),color='green',width=1,align='center')
+    ax2.bar(map(date,negList),map(vol,negList),color='red',width=1,align='center')
+
+    ax2.yaxis.set_label_position("right")
+    ax2.set_ylabel('Volume')
+    plt.show()
     return plt.gcf()
+
 
 def smaPlot(s,N=20,fromDate=None,toDate=None):
     return stockPlot(s,"Adj Close",fromDate,toDate,True,N)
     
 if __name__ == "__main__":
-    Google = stockInfo("GOOG",date(2000,1,1),date.today())
-    s = smaPlot(Google)
+    Google = stockInfo("GOOG",date(2012,1,1),date.today())
+    s = stockPlot(Google,'Adj Close')
     s.show()
